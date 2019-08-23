83a7f9edf0d27f6353b2ddf436b3e494d0a2c7e4
sunitra <sunitra1525@gmail.com>
2019-08-23 16:12:30 +0700

    no message

--- chemprojects/settings.py
+++ chemprojects/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'chemprojects/static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'chemprojects/static')
+]
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
